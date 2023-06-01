9adee83902caf01fb34a48a920a64f5511b8d372
WORK <WORK@DESKTOP-K1M21UD>
2023-06-02 02:02:10 +0530

    Edit function not working

--- ECalendar-App/resources/views/customers/edit.blade.php
+++ ECalendar-App/resources/views/customers/edit.blade.php
@@ -5,10 +5,22 @@
 <div class="card">
     <div class="card-header"> Some Page</div>
     <div class="card-body">
-        <form action="{{url('customer/'.$customer->id)}}" method="post">
+        <form action="{{url('customer/'.$customers->id)}}" method="post">
             {!! csrf_field() !!}
             @method("PATCH")
-            <input type="hidden" name="id" id="id" value="{{$customer->id}}" id="id" /> 
+            <input type="hidden" name="id" id="id" value="{{$customers->id}}" id="id" /> 
+            <label> First Name</label></br>
+            <input type="text" name="firstname" id="firstname" value="{{$customers->firstname}}" class="form-control"></br>
+            <label>Last Name</label></br>
+            <input type="text" name="lastname" id="lastname" value="{{$customers->lastname}}" class="form-control"></br>
+            <label>Address</label></br>
+            <input type="text" name="address" id="address" value="{{$customers->address}}" class="form-control"></br>
+            <label> Email</label></br>
+            <input type="text" name="email" id="email" value="{{$customers->email}}" class="form-control"></br>
+            <label> Telephone</label></br>
+            <input type="text" name="telephone" id="telephone" value="{{$customers->telephone}}" class="form-control"></br>
+        
+            <input type="submit" value="Update" class="btn btn-success"></br>
         
 {{-- issues are here wipe all and start again --}}
         </form>
